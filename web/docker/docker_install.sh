960bfe65c312f07607435349f2899cf5837ade2b
fwenhua <langrenbule@gmail.com>
2019-11-22 17:59:39 +0800

    add  os judge

--- web/docker/docker_install.sh
+++ web/docker/docker_install.sh
@@ -3,46 +3,41 @@
 function docker_remove(){
     read -p "是否卸载重装(y/n)" -n 1  choice
     if [ $choice = "Y" -o $choice = "y" ]; then
-        osReleas = ../../common/os_releas.sh 
-        echo "当前系统为:$osReleas"
-        case $osReleas in
-            ubuntu)
+        source /etc/os-release $ID
+        echo "当前系统为:$ID"
+        case $ID in
+            debian|ubuntu)
                 sudo apt-get remove docker docker-engine
-                if [ $? -eq 0 ]; then
-                    rm -fr /var/lib/docker/
-                    if [ $? -eq 0 ]; then
-                        echo "卸载完成,即将重新安装"
-                        docker_install
-                    else
-                        echo "卸载docker失败,请手动卸载【2】"
-                    fi
-                else
-                    echo "卸载docker失败,请手动卸载【1】"
-                fi
                 ;;
-            centos)
+            centos|rhel)
                 sudo yum remove docker \
                     docker-common \
                     container-selinux \
                     docker-selinux \
+                    docker-engine    
+                ;;
+            fedora)
+                sudo dnf remove docker \
+                    docker-common \
+                    container-selinux \
+                    docker-selinux \
                     docker-engine
-                if [ $? -eq 0 ]; then
-                    rm -fr /var/lib/docker/
-                    if [ $? -eq 0 ]; then
-                        echo "卸载完成,即将重新安装"
-                        docker_install
-                    else
-                        echo "卸载docker失败,请手动卸载【2】"
-                    fi
-                else
-                    echo "卸载docker失败,请手动卸载【1】"
-                fi
                 ;;
             *)
                 exit 1
                 ;;
         esac
-        
+        if [ $? -eq 0 ]; then
+            rm -fr /var/lib/docker/
+            if [ $? -eq 0 ]; then
+                echo "卸载完成,即将重新安装"
+                docker_install
+            else
+                echo "卸载docker失败,请手动卸载【2】"
+            fi
+        else
+            echo "卸载docker失败,请手动卸载【1】"
+        fi
     else
         echo "docker已取消重装"
     fi
