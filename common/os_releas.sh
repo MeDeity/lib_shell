7cd68d664b3cd8b52ed8663aa911e4a4ef331d11
fwenhua <langrenbule@gmail.com>
2019-11-22 17:45:00 +0800

    add  os judge

--- common/os_releas.sh
+++ common/os_releas.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-source /etc/os-release
-case $ID in
-    debian)
-        return debian
-        ;;
-    ubuntu)
-        return ubuntu
-        ;;
-    devuan)
-        return devuan
-        ;;
-    centos)
-        return centos
-        ;;
-    fedora)
-        return fedora
-        ;;
-    rhel)
-        return rhel
-        ;;
+function obtainOS(){
+    source /etc/os-release
+    case $ID in
+        debian)
+            return debian
+            ;;
+        ubuntu)
+            return ubuntu
+            ;;
+        devuan)
+            return devuan
+            ;;
+        centos)
+            return centos
+            ;;
+        fedora)
+            return fedora
+            ;;
+        rhel)
+            return rhel
+            ;;
 
-    *)
-        exit 1
-        ;;
-esac
+        *)
+            exit 1
+            ;;
+    esac
+}
