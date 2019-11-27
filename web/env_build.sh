b3b630b2dee92bedda422e743cd92c02c6c628fb
fwenhua <langrenbule@gmail.com>
2019-11-27 13:53:40 +0800

    update env.sh

--- web/env_build.sh
+++ web/env_build.sh
@@ -5,12 +5,17 @@ function menu(){
     echo "环境搭建脚本"
     echo "1.docker 安装"
     echo "2.docker 重装"
+    echo "3.脚本初始化"
+    echo "4.批量安装容器"
+    echo "5.批量停止容器"
+    echo "6.批量移除容器"
     echo "create by MeDeity(langrenbule@gmail.com) 2019-11-22 15:38:46"
     echo "===================="
 }
 
 function handleInput(){
   source ./docker/docker_install.sh
+  source ./docker-compose.sh
   case $1 in
     1)
       docker_install
@@ -19,8 +24,17 @@ function handleInput(){
       docker_remove
       ;;
     3)
-      
+      docker-compose init
       ;;
+    4)
+      docker-compose start
+      ;;
+    5)
+      docker-compose stop
+      ;;  
+    6)
+      docker-compose rm
+      ;;    
     *)
       exit 1
       ;;
